eae33b46228f93c767ff3cba518f5015f3eca04f
SamuelAvella <saveper1907@g.educaand.es>
2023-12-13 10:29:30 +0100

    update

---
+++ js/function.js
@@ -0,0 +1,16 @@
+/********************************************************************************/
+/*                                 JS of index.html                             */
+/*                            Author: Samuel Avella Pérez                       */
+/*                                    Version: 1.0                              */
+/*                               Comment: final project                         */
+/********************************************************************************/
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
